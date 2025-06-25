27f276d264d6765b89c6c60bc4e896be131e6401
Banyeh Akika <banyehakika81@gmail.com>
2025-06-25 08:18:27 +0200

    Defined ports

--- server/app.js
+++ server/app.js
@@ -1,12 +1,16 @@
 import express from 'express'
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 import authRoute from './routes/index.js'
 
-app.use(express.json())
-app.use(authRoute)
+app.use(express.json());
 
+app.use('/api/auth', authRoute);
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the API! Use /api/auth for authentication routes.')
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
