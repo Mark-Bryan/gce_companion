cb45a9292834aa9feea887fd17f4c71a393384af
Banyeh Akika <banyehakika81@gmail.com>
2025-06-30 00:51:42 +0200

    Added CORS

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 import express from 'express'
 const app = express()
 const port = process.env.PORT || 3000
+import cors from 'cors'
 
 import authRoute from './routes/index.js'
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/api/auth', authRoute);
 
@@ -15,7 +17,7 @@ app.get('/', (req, res) => {
 
 if (process.env.RENDER === 'true' || process.env.NODE_ENV !== 'production') {
     app.listen(port, () => {
-        console.log(`Server is running in development mode on port ${port}`);
+        console.log(`Server is running  on port ${port}`);
     })
 }
 
